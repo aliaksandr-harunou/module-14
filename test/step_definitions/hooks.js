44b37be30bf67286d2dae4be26c91fd470d775a7
Aliaksandr Harunou <Aliaksandr_Harunou@epam.com>
2022-04-15 17:55:32 +0300

    add condition for screenshots attachment

--- test/step_definitions/hooks.js
+++ test/step_definitions/hooks.js
@@ -1,9 +1,11 @@
-const { After } = require('@cucumber/cucumber');
+const { After, Status } = require('@cucumber/cucumber');
 const { browser } = require('protractor');
 
 
-After(async function () {
-    const screenshot = await browser.takeScreenshot();
-    const decodedImage = new Buffer.from(screenshot, 'base64');
-    return this.attach(decodedImage, 'image/png'); 
+After(async function (testCase) {
+    if (testCase.result.status === Status.FAILED) {
+        const screenshot = await browser.takeScreenshot();
+        const decodedImage = new Buffer.from(screenshot, 'base64');
+        return this.attach(decodedImage, 'image/png'); 
+    }    
 });
